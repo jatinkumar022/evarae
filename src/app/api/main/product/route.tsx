ac68d336c91360f5e1b054699cedd576cc8a782c
jatinkumar022 <jatinrmn@gmail.com>
2025-09-26 16:10:48 +0530

    order cart api calls done

--- src/app/api/main/product/route.tsx
+++ src/app/api/main/product/route.tsx
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { connect } from '@/dbConfig/dbConfig';
 import Product from '@/models/productModel';
+import '@/models/categoryModel';
 
 export async function GET(request: Request) {
   try {
